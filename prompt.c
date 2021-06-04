b297b9e28b564691bdfd60c1ac1588b655816ab5
oluwaseun <lere.akinwunmi@gmail.com>
2021-06-05 00:15:16 +0100

    Add line history and input arrow key movement with editline

--- prompt.c
+++ prompt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Declare a buffer for user input of size 2048
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char **argv)
 {
@@ -10,12 +11,14 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-		fputs("lispy> ", stdout);
+		char *input = readline("lispy> ");
 
-		// Read a line of user input of maximum size 2048
-		fgets(input, 2048, stdin);
+		add_history(input);
 
-		printf("No you're a %s", input);
+		printf("No you're a %s\n", input);
+
+		// Free received input
+		free(input);
 	}
 
 	return 0;
